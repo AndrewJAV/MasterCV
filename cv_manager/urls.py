25eda9fd6c5ec28ca207762d92d772ba5bda4337
Andres <andres.jar2127@gmail.com>
2024-08-30 14:19:07 -0600

    end

--- cv_manager/urls.py
+++ cv_manager/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('person/<slug:slug>/', views.cv_detail, name='cv_view'),
+    path('cv/<slug:slug>/', views.cv_detail, name='cv_view'),
     path('search/', views.search, name='search')
 ]
